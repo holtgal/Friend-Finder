6973a3ec422992696e88bf441f0449b48cfb3b34
Holt Galbraith <holtgalbraith@Holts-MacBook-Pro.local>
2019-07-10 18:59:25 -0600

    working, didnt commit much, was using local host

---
+++ App/Data/friends.js
@@ -0,0 +1,55 @@
+var friends = [
+	
+	{
+		name: "George Oscar Bluth",
+		photo: "http://3.bp.blogspot.com/_-aqc14tIy6E/SbDYCpym_CI/AAAAAAAAABo/8RDy2rgv8ds/s320/george+bluth.JPG",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Lucille Bluth",
+		photo: "https://mondrian.mashable.com/uploads%252Fstory%252Fthumbnail%252F72878%252F079e38a9-f76a-4ef3-8a86-af03bf6215f5.png%252F950x534.png?signature=D_6OTz_gxn0WHefrKnEEK2BE4lk=&source=https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Michael Bluth",
+		photo: "https://uproxx.files.wordpress.com/2015/01/michael-bluth-reactions.jpg?quality=100&w=650",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	},
+	
+	
+];
+
+module.exports = friends;
